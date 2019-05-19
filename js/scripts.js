17688f541a1b00054bced4f8d4e0e200303ca4c2
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-05-19 11:40:15 +0200

    Update scripts.js
    Created three more Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 var repository = [
     { 
         name: "Bulbasaur", 
-        height: 7, 
+        height: 0.7, 
         types: ['grass', 'poison']
+    },
+    { 
+        name: "Charizard", 
+        height: 1.7, 
+        types: ['fire', 'flying']
+    },
+    { 
+        name: "CharMetapod", 
+        height: 0.7, 
+        types: ['bug']
+    },
+    { 
+        name: "Pidgey", 
+        height: 0.3, 
+        types: ['flying','normal']
     }
   ];
