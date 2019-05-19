e376af3fd2f624e54c6982a8b30e2b9a5e0a522a
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-05-19 20:53:18 +0200

    js\scripts.js
    rewrote array and for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,17 @@
-var repository = [
-    { 
-        name: "Bulbasaur", 
-        height: 0.7, 
-        types: ['grass', 'poison']
-    },
-    { 
-        name: "Charizard", 
-        height: 1.7, 
-        types: ['fire', 'flying']
-    },
-    { 
-        name: "CharMetapod", 
-        height: 0.7, 
-        types: ['bug']
-    },
-    { 
-        name: "Pidgey", 
-        height: 0.3, 
-        types: ['flying','normal']
+var repository = [ 
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    {name: 'Charizard', height: 17, types: ['fire', 'flying'] },
+    {name: 'CharMetapod', height: 7, types: ['bug',] },
+    {name: 'Pidgey', height: 3, types: ['flying', 'normal'] }
+];
+
+ for (var i=0; i < repository.length; i++) {
+    if (repository[i].height >= 7) {
+        document.write(repository[i].name + ' , height: '  + repository[i].height  + ' - Wow, that’s big! ' + '<br>');
+
+    } else {
+        document.write(repository[i].name + ' , height: ' + repository[i].height  + '<br>');
     }
-  ];
+}
+
+document.write(repository.name[1]);
