aaacbe67ab83625173d0614b99f1a4bfc9e14544
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-05-28 20:14:41 +0200

    js\scripts.js
    change repository
    
     to use forEach function to loop array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 var repository = [ 
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    {name: 'Charizard', height: 17, types: ['fire', 'flying'] },
-    {name: 'CharMetapod', height: 7, types: ['bug',] },
-    {name: 'Pidgey', height: 3, types: ['flying', 'normal'] }
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    {name: 'CharMetapod', height: 0.7, types: ['bug',] },
+    {name: 'Pidgey', height: 0.3, types: ['flying', 'normal'] }
 ];
 
- for (var i=0; i < repository.length; i++) {
-    if (repository[i].height >= 7) {
-        document.write(repository[i].name + ' , height: '  + repository[i].height  + ' - Wow, that’s big! ' + '<br>');
+repository.forEach(function(pokemonList){
+    if (pokemonList.height >= 0.7) {
+        document.write(pokemonList.name + ' , height: '  + pokemonList.height  + ' - Wow, that’s big! ' + '<br>');
 
     } else {
-        document.write(repository[i].name + ' , height: ' + repository[i].height  + '<br>');
+        document.write(pokemonList.name + ' , height: ' + pokemonList.height  + '<br>');
+    }
+  });
+
+
+  
+/*
+var repository2 = [ 
+    {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
+    {name: 'Squirtle', height: 0.5, types: ['water',] },
+    {name: 'Butterfree', height: 1.1, types: ['bug', 'flying'] },
+    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal'] }
+];
+
+function printArrayDetails(pokemonList){
+    document.write('<h2>Pokemon List</h2>');
+ for (var i=0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 0.7) {
+        document.write(pokemonList[i].name + ' , height: '  + pokemonList[i].height  + ' - Wow, that’s big! ' + '<br>');
+
+    } else {
+        document.write(pokemonList[i].name + ' , height: ' + pokemonList[i].height  + '<br>');
     }
 }
+    
+}
 
-document.write(repository.name[1]);
+printArrayDetails(repository);
+printArrayDetails(repository2);
+
+*/
